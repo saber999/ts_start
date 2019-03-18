e2bd051187d42c7c52d01fc19105385ea41ae992
jone <876573176@qq.com>
2019-03-18 20:06:32 +0800

    first commit

---
+++ src/package/index.ts
@@ -0,0 +1,29 @@
+import errTip from './errTips.vue';
+const TIPS = {
+    durationDefault: 3,
+    install(Vue: any) {
+        Vue.component('errTips', errTip);
+        function initMsg(msg: string, duration?: number, callback?: object): void{
+            let currDuration: number = duration ? duration : TIPS.durationDefault;
+            let vueTips = Vue.extend({
+                render(h: Vue.CreateElement) {
+                    return h(errTip, {
+                        props: {
+                            errMsg: msg,
+                            duration: currDuration,
+                        },
+                    });
+                },
+            }); 
+            let newTips = new vueTips();
+            let vm = newTips.$mount();
+            document.body.appendChild(vm.$el);
+            vm.handleHide();
+            let t1 = setTimeout(() => {
+                clearTimeout(t1);
+                newTips.$destroy();
+                document.body.removeChild(vm.$el);
+            }, currDuration);
+        }
+    }
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <errTips :errMsg = "msg"></errTips>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import errTips from '@/package/errTips.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+    errTips
+  },
+})
+export default class Home extends Vue {
+  msg:String = 'tesRQtdaaDt'
+}
+</script>
